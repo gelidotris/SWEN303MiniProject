bc84fb846fce91d93bb9bd4b480ba5d5919ff8c4
Trisha Gelido <shazzeeiz910@yahoo.co.nz>
2016-03-21 19:50:23 +1300

    Added files to git

---
+++ public/javascript.js
@@ -0,0 +1,23 @@
+function homepage(){
+	window.location= "index.html"
+}
+
+function addDoc(){
+		window.location = "addDocument.html"
+}
+
+function exploreDocType(){
+		window.location = "exploreDocType.html"
+}
+
+function exploreAuthor(){
+		window.location = "exploreAuthor.html"
+}
+
+function exploreYear(){
+		window.location = "exploreYear.html"
+}
+
+function colensoProject(){
+		window.location = "http://www.williamcolenso.co.nz/"
+}
