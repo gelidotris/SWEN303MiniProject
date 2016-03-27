d424c059a6d3b2bebc79bd916eda155963edccb3
Trisha Gelido <shazzeeiz910@yahoo.co.nz>
2016-03-27 18:03:32 +1300

    Converted html files to jade
    - Also added basex and Colenso folders to project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Colenso Project' });
 });
+
+function homepage(){
+	window.location= "index.html"
+}
+
+function addDoc(){
+		window.location = "addDocument.html"
+		window.startBasex();
+}
+
+function exploreDocType(){
+		window.location = "exploreDocType.html"
+}
+
+function exploreAuthor(){
+		window.location = "exploreAuthor.html"
+}
+
+function exploreYear(){
+		window.location = "exploreYear.html"
+}
+
+function colensoProject(){
+		window.location = "http://www.williamcolenso.co.nz/"
+}
 
 module.exports = router;
