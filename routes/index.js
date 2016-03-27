bd8d86f1f06c8c7cc12145f6f4e6c9889815d388
Trisha Gelido <shazzeeiz910@yahoo.co.nz>
2016-03-27 22:05:43 +1300

    Minor fixes + added Search page
    - Fixed up links to navbar
    - Created Search results page

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,13 +10,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colenso Project' });
 });
 
+router.get('/addDocument', function(req, res) {
+  res.render('addDocument', { title: 'Colenso Project >> Add Document' });
+
+});
+
+router.get('/exploreAuthor', function(req, res) {
+  res.render('exploreAuthor', { title: 'Colenso Project >> Explore Author' });
+});
+
+router.get('/exploreDocType', function(req, res) {
+  res.render('exploreDocType', { title: 'Colenso Project >> Explore Document Type' });
+});
+
+router.get('/search', function(req, res) {
+	res.render('search', { title: 'Colenso Project >> Search', content: req.query.searchString });
+});
 function homepage(){
 	window.location= "index.html"
 }
 
 function addDoc(){
-		window.location = "addDocument.html"
-		window.startBasex();
+		window.location.href = "/addDocument"
 }
 
 function exploreDocType(){
@@ -28,7 +47,7 @@ function exploreYear(){
 }
 
 function colensoProject(){
-		window.location = "http://www.williamcolenso.co.nz/"
+		window.location.href = "http://www.williamcolenso.co.nz/"
 }
 
 module.exports = router;
