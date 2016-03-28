cacfc87b146cf1e0f5660c2e3b74c4b152742d54
Trisha Gelido <shazzeeiz910@yahoo.co.nz>
2016-03-28 22:02:35 +1300

    Added Search function + Display 1 xml file
    - Able to display list of file titles which contain a given string
    - Can view an xml file

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ client.execute("OPEN Colenso");
 var express = require('express');
 var router = express.Router();
 
+var xmlFilePath = this.href;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colenso Project' });
 });
 
 router.get('/addDocument', function(req, res) {
-  res.render('addDocument', { title: 'Colenso Project >> Add Document' });
+  res.render('addDocument', { title: 'Add Document' });
 
 });
 
@@ -24,7 +26,63 @@ router.get('/exploreDocType', function(req, res) {
 });
 
 router.get('/search', function(req, res) {
-	res.render('search', { title: 'Colenso Project >> Search', content: req.query.searchString });
+	var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+	"for $x in (collection('Colenso')) where $x[contains(.,'"+req.query.searchString+"')] return(<li><a href='/displayDocument'>{$x//title/text()}</a></li>)";
+	xmlFilePath = "for $x in (collection('Colenso')) where $x[contains(.,'"+req.query.searchString+"')] return(<li><a href='/displayDocument'>{$x//title/text()}</a></li>)";
+
+	client.execute(query,
+	function (error, result) {
+		if(error){ console.error(error);}
+		else {
+			res.render('search', { title: 'Search Results', search_results: result.result, search_Text: req.query.searchString});
+			console.log("RESULT");
+			}
+		}
+		);
+
+});
+
+router.get('/displayDocument', function(req, res) {
+	var docu = doc('Colenso/'+ xmlFilePath + ')';
+	var query = "XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
+	String(docu);
+	client.execute(query,
+	function (error, result) {
+		if(error){ console.error(error);}
+		else {
+			res.render('displayDocument', { title: 'Document', display_doc: result.result});
+			console.log("RESULT");
+			}
+		}
+		);
+});
+
+router.get('/docType', function(req, res) {
+	var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+	"collection('Colenso')//title[contains(.,'Diary')]/text()";
+	client.execute(query,
+	function (error, result) {
+		if(error){ console.error(error);}
+		else {
+			res.render('docType', { title: 'Colenso Project >> Diaries', document_type: result.result});
+			console.log("RESULT");
+			}
+		}
+		);
+});
+
+router.get('/browse', function(req, res) {
+	var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+		"doc('Colenso/McLean/private_letters/PrLMcL-0024.xml')";
+		client.execute(query,
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				res.render('browse', { title: 'The Colenso Project', list_letters: result.result });
+				console.log("RESULT");
+			}
+		}
+		);
 });
 
 module.exports = router;
