76fbc47af59a26168bf56df93f33d9eb77c8ff53
Trisha Gelido <shazzeeiz910@yahoo.co.nz>
2016-03-29 15:19:40 +1300

    Able to redirect to file from search results
    - Can redirect to view page from search results page after clicking a
    link + displays that file
    - Minor css fixes

--- routes/index.js
+++ routes/index.js
@@ -7,65 +7,71 @@ var router = express.Router();
 
 var xmlFilePath = this.href;
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Colenso Project' });
+	res.render('index', { title: 'Colenso Project' });
 });
 
 router.get('/addDocument', function(req, res) {
-  res.render('addDocument', { title: 'Add Document' });
+	res.render('addDocument', { title: 'Add Document' });
 
 });
 
 router.get('/exploreAuthor', function(req, res) {
-  res.render('exploreAuthor', { title: 'Colenso Project >> Explore Author' });
+	res.render('exploreAuthor', { title: 'Colenso Project >> Explore Author' });
 });
 
 router.get('/exploreDocType', function(req, res) {
-  res.render('exploreDocType', { title: 'Colenso Project >> Explore Document Type' });
+	res.render('exploreDocType', { title: 'Colenso Project >> Explore Document Type' });
 });
 
 router.get('/search', function(req, res) {
 	var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-	"for $x in (collection('Colenso')) where $x[contains(.,'"+req.query.searchString+"')] return(<li><a href='/displayDocument'>{$x//title/text()}</a></li>)";
-	xmlFilePath = "for $x in (collection('Colenso')) where $x[contains(.,'"+req.query.searchString+"')] return(<li><a href='/displayDocument'>{$x//title/text()}</a></li>)";
-
-	client.execute(query,
-	function (error, result) {
-		if(error){ console.error(error);}
-		else {
-			res.render('search', { title: 'Search Results', search_results: result.result, search_Text: req.query.searchString});
-			console.log("RESULT");
+	"for $x in (collection('Colenso')) where $x[contains(.,'"+req.query.searchString+"')] let $p := db:path($x) return(<li><a href='/viewDocument?doc={$p}'>{$x//title/text()}</a></li>)";
+	if (req.query.searchString){
+		client.execute(query,
+			function (error, result) {
+				if(error){ console.error(error);}
+				else {
+					res.render('search', { title: 'Search Results', search_results: result.result, search_Text: req.query.searchString});
+					console.log("RESULT");
+				}
 			}
-		}
-		);
-
+			);
+	}
+	else {res.render('search', { title: 'Search Results', search_results: "", search_Text: ""});
+	console.log("RESULT");
+}
 });
 
-router.get('/displayDocument', function(req, res) {
-	var docu = doc('Colenso/'+ xmlFilePath + ')';
-	var query = "XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
-	String(docu);
-	client.execute(query,
-	function (error, result) {
-		if(error){ console.error(error);}
-		else {
-			res.render('displayDocument', { title: 'Document', display_doc: result.result});
-			console.log("RESULT");
+router.get('/viewDocument', function(req, res) {
+	if(req.query.doc){
+		client.execute("XQUERY doc('Colenso/"+req.query.doc+"')", 
+			function(error, result){
+				if(error){ 
+					console.error(error);}
+				else {
+					res.render('viewDocument', { title: 'Colenso Project', display_doc: result.result});
+				}
+
 			}
-		}
-		);
+			);
+	}
+	else {
+		 res.render('viewDocument', { display_doc: req.query.doc });
+	}
 });
 
 router.get('/docType', function(req, res) {
 	var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
 	"collection('Colenso')//title[contains(.,'Diary')]/text()";
 	client.execute(query,
-	function (error, result) {
-		if(error){ console.error(error);}
-		else {
-			res.render('docType', { title: 'Colenso Project >> Diaries', document_type: result.result});
-			console.log("RESULT");
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				res.render('docType', { title: 'Colenso Project >> Diaries', document_type: result.result});
+				console.log("RESULT");
 			}
 		}
 		);
@@ -73,8 +79,8 @@ router.get('/docType', function(req, res) {
 
 router.get('/browse', function(req, res) {
 	var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-		"doc('Colenso/McLean/private_letters/PrLMcL-0024.xml')";
-		client.execute(query,
+	"for $x in (collection('Colenso/Hooker')) where $x[contains(.,'1863')] return db:path($x)";
+	client.execute(query,
 		function (error, result) {
 			if(error){ console.error(error);}
 			else {
